1df274d3b7f5a63520453e13e20fef76fc6ce38d
Austin Moody <amoody25@uncc.edu>
2020-10-27 20:01:53 -0400

    Added insertion feature to budget

--- budget_schema.js
+++ budget_schema.js
@@ -1,22 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const budgetSchema = new mongoose.Schema({
+const budgetSchema = new mongoose.Schema(
+  {
     id: {
-        type: Number,
-        required: true,
-        unique: true,
+      type: Number,
+      required: true,
+      unique: true,
     },
-    value: {
-        type: Number,
-        trim: true,
-        required: true,
-        uppercase: true,
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    budget: {
+      type: Number,
+      trim: true,
+      required: true,
+      uppercase: true,
     },
     color: {
-        type: String,
-        trim: true,
-        uppercase: true
-    }
-}, { collection: 'budgetValues' });
+      type: String,
+      trim: true,
+      uppercase: true,
+    },
+  },
+  { collection: "budgetValues" }
+);
 
-module.exports = mongoose.model('budget', budgetSchema);
+module.exports = mongoose.model("budget", budgetSchema);
--- server.js
+++ server.js
@@ -1,46 +1,52 @@
 //Budget API
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const budgetModel = require("./budget_schema");
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const budget_Schema = require("./budget_schema");
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
-let url = 'mongodb://localhost:27017/budget';
+let url = "mongodb://localhost:27017/budget";
 
-app.get('/budget', (req, res) => {
-    mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true })
-            .then(() => {
-                console.log("Connected to the database for budget ");
-                budgetModel.find({})
-                           .then((data) => {
-                               res.send(data);
-                               mongoose.connection.close();
-                           })
-                           .catch((connectionError) => {
-                               console.log(connectionError);
-                           })
-            });
+app.get("/budget", (req, res) => {
+  mongoose
+    .connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connected to the database for budget ");
+      budgetModel
+        .find({})
+        .then((data) => {
+          res.send(data);
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          console.log(connectionError);
+        });
+    });
 });
-app.get('/update', (req, res, document) => {
-    mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true })
-            .then(() => {
-                console.log("Connected to the database for upsertion")
-                let newData = {$set: {id: idVar, value: valueVar}};;
-                budgetModel.updateOne({id: idVar, newData})
-                           .then((data) => {
-                               console.log(data);
-                               mongoose.connection.close();
-                           })
-                           .catch((connectionError) => {
-                               console.log(connectionError);
-                           })
-            });
 
-})
+app.post("/insert", (req, res) => {
+  mongoose.connect(
+    url,
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  );
+  console.log(req.body);
+  budgetModel
+    .insertMany(req.body)
+    .then((data) => {
+        res.send("Insertion completed");
+      mongoose.connection.close();
+    })
+    .catch((connectionError) => {
+      console.log(connectionError);
+    });
+});
 
 // const budget = {
 //   myBudget: [
@@ -64,5 +70,5 @@ app.get('/update', (req, res, document) => {
 // });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`API served at http://localhost:${port}`);
 });
